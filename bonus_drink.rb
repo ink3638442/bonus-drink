e6c9d0d3cb8b5e4f25da66c5430dc36b8b5d7cc3
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-03 02:16:08 +0900

    Add Test_case amount 11

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,6 +1,21 @@
 class BonusDrink
   def self.total_count_for(amount)
     bonus_bottle = amount / 3
-    amount += bonus_bottle
+    total_bottle = amount + bonus_bottle
+
+    # さらに入手可能かを判断するため、余りのボトルとボーナスボトルの合計を数える
+    remainder_bottle = amount % 3
+    bottle_count     = bonus_bottle + remainder_bottle
+
+    # 入手不可になるまで繰り返す
+    while bottle_count >= 3
+      bonus_bottle  = bottle_count / 3
+      total_bottle += bonus_bottle
+
+      remainder_bottle = bottle_count % 3
+      bottle_count     = bonus_bottle + remainder_bottle
+    end
+
+    return total_bottle
   end
 end
